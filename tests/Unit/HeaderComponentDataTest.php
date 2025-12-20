5b4e38f2bf164edfa66ea79271dd7f9f4eafc00e
Chancel <cshongo33@gmail.com>
2025-12-21 01:42:57 +0200

    all the headercomponent tests

--- tests/Unit/HeaderComponentDataTest.php
+++ tests/Unit/HeaderComponentDataTest.php
@@ -14,4 +14,25 @@ class HeaderComponentDataTest extends TestCase
         $this->assertEquals("Hello World!", $data["text"]);
         $this->assertEquals($data['format'], HeaderFormats::TEXT->value);
     }
+
+    public function testImageHeaderComponentData(): void
+    {
+        $data = HeaderComponentData::image("https://cweetagram.com/logo.png")->toArray();
+        $this->assertEquals("https://cweetagram.com/logo.png", $data["example"]['header_handle']);
+        $this->assertEquals($data['format'], HeaderFormats::IMAGE->value);
+    }
+
+    public function testImageVideoComponentData(): void
+    {
+        $data = HeaderComponentData::video("https://cweetagram.com/logo.mp4")->toArray();
+        $this->assertEquals("https://cweetagram.com/logo.mp4", $data["example"]['header_handle']);
+        $this->assertEquals($data['format'], HeaderFormats::VIDEO->value);
+    }
+
+    public function testImageDocumentComponentData(): void
+    {
+        $data = HeaderComponentData::document("https://cweetagram.com/logo.pdf")->toArray();
+        $this->assertEquals("https://cweetagram.com/logo.pdf", $data["example"]['header_handle']);
+        $this->assertEquals($data['format'], HeaderFormats::DOCUMENT->value);
+    }
 }
